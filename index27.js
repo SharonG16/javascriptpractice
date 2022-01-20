8a7f303cffbba879d1272387aa0822cbac099195
SharonG16 <sfbaylon@yahoo.com>
2022-01-20 11:59:20 -0700

    examples

--- index27.js
+++ index27.js
@@ -5,4 +5,3 @@ console.log( `I add 2 + 3 and the result is ${firstNumber + secondNumber}`);
 console.log( `I subtract 2 - 3 and the result is ${firstNumber - secondNumber}`);
 console.log( `I multiply 2 * 3 and the result is ${firstNumber * secondNumber}`);
 console.log( `I divide 2 / 3 and the result is ${firstNumber / secondNumber}`);
-console.log( `The remainder of 2 % 3 and the result is ${firstNumber % secondNumber}`);
